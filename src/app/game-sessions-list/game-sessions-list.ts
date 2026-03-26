62ce8b4d1eab70131f2c337bf78d2518047ce811
MarcoBisco <marco19febbraio@gmail.com>
2026-03-26 10:24:39 +0100

    Importazione mockup, creazione componente GameSessionsList e inizio HomePage

---
+++ src/app/game-sessions-list/game-sessions-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-sessions-list',
+  imports: [],
+  templateUrl: './game-sessions-list.html',
+  styleUrl: './game-sessions-list.css',
+})
+export class GameSessionsList {
+
+}

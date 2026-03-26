62681775d92b54230d86cf6b27767b3457194285
MarcoBisco <marco19febbraio@gmail.com>
2026-03-26 09:56:35 +0100

    Creazione componenti, servizi e creazione metodo getGameSessions() su GameSessionService

---
+++ src/app/game-session-preview/game-session-preview.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-session-preview',
+  imports: [],
+  templateUrl: './game-session-preview.html',
+  styleUrl: './game-session-preview.css',
+})
+export class GameSessionPreview {
+
+}

09e7c23cdc686a59d2101a076ee5fbc33aafd1af
Chiara <chiara@esempio.it>
2026-03-26 13:02:41 +0100

    css e html di game-session-preview e list

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GameSessionsList } from './game-sessions-list/game-sessions-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [GameSessionsList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/game-session-preview/game-session-preview.ts
+++ src/app/game-session-preview/game-session-preview.ts
@@ -1,17 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, input, output } from '@angular/core';
 import { GameSession } from '../model/entities';
-import { GameService } from '../service/game-service';
-import { PubTableService } from '../service/pub-table-service';
 
 @Component({
   selector: 'app-game-session-preview',
-  imports: [FormsModule,CommonModule],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './game-session-preview.html',
   styleUrl: './game-session-preview.css',
 })
 export class GameSessionPreview {
   gameSession = input.required<GameSession>();
 
+  // Evento per gestire il click sulla card (es. per aprire il dettaglio)
+  selectSession = output<GameSession>();
+
+  onDetailsClick() {
+    this.selectSession.emit(this.gameSession());
+  }
 }
